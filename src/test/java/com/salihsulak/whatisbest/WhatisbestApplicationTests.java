a908dc7a8e19b60cbe3fc3050f8c8f920218bfb2
Salih Sulak <salihsulak00@gmail.com>
2022-10-30 12:41:18 +0300

    initial commit

---
+++ src/test/java/com/salihsulak/whatisbest/WhatisbestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salihsulak.whatisbest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatisbestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
